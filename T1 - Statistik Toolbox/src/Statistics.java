a3d79bee52ef6e2ed4e1c26386f65ba7fd81664a
marcelborbus <m@borbus.eu>
2024-10-31 15:51:26 +0100

    Initial commit

---
+++ T1 - Statistik Toolbox/src/Statistics.java
@@ -0,0 +1,121 @@
+public class Statistics {
+  public static float variance(int[] x, int n) {
+    float sum = 0;
+    float variance = 0;
+
+    /* Alle Zahlen des Arrays addieren und anschließend durch die
+    die Anzahl der Werte teilen um den Mittelwert zu berechnen */
+    for (int i = 0; i < n; i++) sum += x[i];
+    float mean = sum / n;
+
+    /* Den Mittelwert vom i-ten Wert des Arrays subtrahieren und
+    das Ergebnis quadrieren. Summe der Quadrate bilden und anschließend
+    durch die Anzahl der Werte teilen. */
+    for (int i = 0; i < n; i++) {
+      variance += ((x[i] - mean) * (x[i] - mean));
+    }
+
+    variance /= n;
+
+    return variance;
+  }
+
+  public static int sum(int[] x) {
+    int sum = 0;
+    for (int i = 0; i < x.length; i++) sum += x[i];
+    return sum;
+  }
+
+  public static float standardDeviation(int[] x, int n) {
+
+    float sum = sum(x);
+    float variance = 0;
+
+    /* Alle Zahlen des Arrays addieren und anschließend durch die
+    die Anzahl der Werte teilen um den Mittelwert zu berechnen */
+    float mean = sum / n;
+
+    /* Den Mittelwert vom i-ten Wert des Arrays subtrahieren und
+    das Ergebnis quadrieren. Summe der Quadrate bilden und anschließend
+    durch die Anzahl der Werte teilen. */
+    for (int i = 0; i < n; i++) variance += ((x[i] - mean) * (x[i] - mean));
+    variance /= n;
+
+    // Quadratwurzel der Varianz ergibt Standardabweichung
+    float stddev = variance;
+    float error = 0.0001f; // Fehlertoleranz
+    while ((stddev - (variance / stddev)) > error) stddev = (stddev + (variance / stddev)) / 2;
+
+    return stddev;
+  }
+
+  public static void main(String[] args) {
+    final String invalidArgsMsg =
+        "Error: Arguments must be valid integers.\nUsage: java Statistics <integer> <integer> ...";
+
+    // wenn es keinen Input gibt, print Error
+    if (args.length == 0) {
+      System.out.println(invalidArgsMsg);
+    }
+
+    int[] data = new int[args.length];
+
+    // wenn Input keine Zahl ist, print Error
+    for (int i = 0; i < args.length; i++) {
+      try { // error handling
+        data[i] = Integer.parseInt(args[i]);
+      } catch (NumberFormatException e) {
+        System.out.println(invalidArgsMsg);
+      }
+    }
+
+    // zähle alle Inputs im Format dataSet[x] = {1, 2} := {<InputZahl>, <Anzahl>}
+    int[][] dataSet = new int[data.length][2]; // unsorted
+    int uniqueCount = 0;
+
+    // wenn dataSet Input enthält ...
+    for (int i = 0; i < data.length; i++) {
+      boolean found = false;
+
+      // ... erhöhe Anzahl um 1 ...
+      for (int j = 0; j < i; j++) {
+        if (dataSet[j][0] == data[i]) {
+          dataSet[j][1] += 1;
+          found = true;
+          break;
+        }
+      }
+
+      // ... sonst füge an letzter Stelle mit Anzahl = 1 hinzu
+      if (!found) {
+        dataSet[uniqueCount][0] = data[i];
+        dataSet[uniqueCount][1] = 1;
+        uniqueCount += 1;
+      }
+    }
+
+    float mean = (float) sum(data) / data.length; // muss ergänzt werden
+    int sum = sum(data); // muss ergänzt werden
+    float standardDeviation = standardDeviation(data, data.length); // Standardabweichung
+    float variance = variance(data, data.length); // Varianz
+
+    System.out.println("Mittelwert: " + mean);
+    System.out.println("Summe: " + sum);
+
+    // Output
+
+    System.out.println("Standardabweichung:" + standardDeviation);
+    System.out.println("Varianz:" + variance);
+    System.out.println("Histogram:");
+
+    for (int i = 0; i < uniqueCount; i++) {
+      System.out.print(dataSet[i][0] + ": ");
+
+      for (int j = 0; j < dataSet[i][1]; j++) {
+        System.out.print('*');
+      }
+
+      System.out.println();
+    }
+  }
+}
---
+++ T2 - Permutationen/src/Permutationen.java
@@ -0,0 +1,40 @@
+class Permutationen {
+  public static void main(String[] args) {
+    generate(args, new int[]{});
+  }
+
+  public static void generate(String[] A, int[] selected) {
+    // wenn wir jeden index einmal verwendet haben ist die permutation komplett
+    if (selected.length == A.length) {
+      for (int i = 0; i < selected.length; i++) {
+        System.out.print(A[selected[i]]);
+      }
+
+      System.out.println(); // newline
+    }
+
+    // wähle einen möglichen nächsten index und berechne damit eine permutation
+    for (int i = 0; i < A.length; i++) {
+      // n! möglichkeiten; n für 1. n-1 für 2. ...
+
+      // wenn der aktuelle index schon ausgewählt ist, überspringe
+      boolean skip = false;
+      for (int j = 0; j < selected.length; j++) {
+        if (i == selected[j]) skip = true;
+      }
+
+      if (skip) continue;
+
+      // sonst
+      int[] newSelected = new int[selected.length + 1];
+
+      for (int j = 0; j < selected.length; j++) {
+        newSelected[j] = selected[j];
+      }
+
+      newSelected[selected.length] = i;
+      generate(A, newSelected);
+    }
+  }
+}
+

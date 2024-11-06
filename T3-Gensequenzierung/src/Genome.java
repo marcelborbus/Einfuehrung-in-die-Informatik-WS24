19423831f70d972c86130f68e7de3f361d6222d7
marcelborbus <m@borbus.eu>
2024-11-06 15:19:30 +0100

    Kommentare Hinzugefügt

--- T3-Gensequenzierung/src/Genome.java
+++ T3-Gensequenzierung/src/Genome.java
@@ -1,91 +1,82 @@
 import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Genome {
 
+  // printe Gen von Start bis Ende
+  public static void ausgabe(String s, int begin, int end) {
+    System.out.println("Start bei " + begin + " tag Ende bei " + end);
 
-    public static void ausgabe(String s, int begin, int end) {
-        for (int i = begin; i <= end; i++) {
-            System.out.print(s.charAt(i));
-        }
-
-        System.out.println();
+    for (int i = begin; i <= end; i++) {
+      System.out.print(s.charAt(i));
     }
 
-    // atg
-    public static void findStart(String genome){
-        for (int i = 0; i < genome.length() - 2; i++) {
-            if (genome.charAt(i) == 'a') {
-                if (genome.charAt(i+1) == 't') {
-                    if (genome.charAt(i+2) == 'g') {
-                        int begin = i;
-                        int end = findEnd(genome, i);
-                        ausgabe(genome, begin, end);
-                        i = end;
-                    }
-                }
-            }
+    System.out.println();
+  }
+
+  // Finde das Startkodon
+  public static void findStart(String genome) {
+    for (int i = 0; i < genome.length() - 2; i++) {
+      if (genome.charAt(i) == 'a') { // Start mit ATG
+        if (genome.charAt(i + 1) == 't') {
+          if (genome.charAt(i + 2) == 'g') {
+            int begin = i;
+            int end = findEnd(genome, i); // suche weiter, wo das letzte Gen aufhört
+            ausgabe(genome, begin, end);
+            i = end;
+          }
         }
+      }
+    }
+  }
+
+  // finde ein mögliches Endkodon
+  public static int findEnd(String genome, int begin) {
+    for (int i = begin; i < genome.length() - 2; i += 3) { // Kodons haben immer Länge 3k
+      if (genome.charAt(i) == 't') { // tag, tga oder taa
+        if (genome.charAt(i + 1) == 'a') {
+          if (genome.charAt(i + 2) == 'g') {
+            return i + 2;
+
+          }
+        }
+
+        if (genome.charAt(i + 1) == 'g') { // Tga
+          if (genome.charAt(i + 2) == 'a') {
+            return i + 2;
+          }
+        }
+
+        if (genome.charAt(i + 1) == 'a') { // Taa
+          if (genome.charAt(i + 2) == 'a') {
+            return i + 2;
+          }
+        }
+      }
     }
 
-    // tag, tga oder taa
-    public static int findEnd(String genome, int begin){
-        for (int i = begin; i < genome.length() - 2; i += 3) {
-            if (genome.charAt(i) == 't') {
-                if (genome.charAt(i+1) == 'a') {
-                    if (genome.charAt(i+2) == 'g') {
-                        System.out.println("Start bei " + begin + " tag Ende bei " + i);
-
-                        return i+2;
-
-                    }
-                }
-
-                if (genome.charAt(i+1)== 'g'){
-                    if (genome.charAt(i+2) == 'a') {
-                        System.out.println("Start bei " + begin + " tga Ende bei " + i);
-                        return i+2;
-                    }
-                }
-
-                if (genome.charAt(i+1)== 'a') {
-                    if (genome.charAt(i + 2) == 'a') {
-                        System.out.println("Start bei " + begin + " taa Ende bei " + i);
-                        return i+2;
-                    }
-                }
-            }
+    return -1; // return falls es kein Ende gibt
+  }
 
 
-        }
-
-        return -1;
+  public static void main(String[] args) { // args[0] = dateiname
+    if (args.length == 0) {
+      System.out.println("Bitte gib eine Datei an");
+      return;
     }
 
+    String genome;
 
-    public static void main(String[] args) throws IOException { // args[0] = dateiname
-        if (args.length == 0) {
-            System.out.println("Bitte gib eine Datei an");
-            return;
-        }
-
-        String genome;
-
-        try {
-            File ecoli = new File(args[0]);
-            Scanner sc = new Scanner(ecoli);
-            genome = sc.useDelimiter("\\Z").next();
-            sc.close();
-        } catch (Exception e) {
-            System.out.println("Datei existiert nicht oder ist besetzt.");
-            return;
-        }
-
-
-        findStart(genome);
-
-        System.out.println();
+    try {
+      File ecoli = new File(args[0]);
+      Scanner sc = new Scanner(ecoli);
+      genome = sc.useDelimiter("\\Z").next();
+      sc.close();
+    } catch (Exception e) {
+      System.out.println("Datei existiert nicht oder ist besetzt.");
+      return;
     }
 
+    findStart(genome);
+  }
 }
